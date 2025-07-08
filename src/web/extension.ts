9b0572855c5cac9a9df66fb883ee6990264fadc6
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-07-08 14:17:00 +1000

    add art + fix script + add light mode support

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -5,29 +5,87 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "duckcode" is now active in the web extension host!');
 	context.subscriptions.push(
 		vscode.window.registerWebviewViewProvider(
-			'sleepyduck.view',
+			'sleepyDuck.view',
 			new DuckViewProvider(context)
 		)
-	);
+		);
+	};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('duckcode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	class DuckViewProvider implements vscode.WebviewViewProvider{
+		constructor(private readonly context: vscode.ExtensionContext) {}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from duckcode in a web extension host!');
-	});
+		resolveWebviewView(webviewView: vscode.WebviewView) {
+		const webview = webviewView.webview;
+		webview.options = {
+		enableScripts: true,
+		localResourceRoots: [vscode.Uri.joinPath(this.context.extensionUri, 'img')]
+		};
+		
+		let sleepLevel = 0; // 0 = awake, 1 = sleepy, 2 = asleep + waiting to start break, 3 = on break
+		let duckImages = ['awake.png', 'sleepy.png', 'asleep.png', 'back.png'];
 
-	context.subscriptions.push(disposable);
+
+		const updateDuck = () => {
+			const themeKind = vscode.window.activeColorTheme.kind; // get current vscode theme
+			if (themeKind === vscode.ColorThemeKind.Dark){
+				duckImages = ['awake-dark.png', 'sleepy-dark.png', 'asleep-dark.png', 'back-dark.png'];
+			}
+			else{
+				duckImages = ['awake.png', 'sleepy.png', 'asleep.png', 'back.png'];
+			}
+			const imageName = duckImages[Math.min(sleepLevel, duckImages.length - 1)]; // prevents it from going out of bounds	
+			const imagePath = webview.asWebviewUri(
+				vscode.Uri.joinPath(this.context.extensionUri, 'img', imageName)
+			);
+			webview.html = getDuckHtml(imagePath.toString(), sleepLevel);
+		};
+
+		updateDuck();
+
+		webview.onDidReceiveMessage((message) => {
+			if (message.command === 'clicked') {
+				if (sleepLevel === 2){
+					sleepLevel = 4;
+				}
+				else if (sleepLevel === 4){
+					sleepLevel = 0;
+				}
+			}
+			updateDuck();
+		});
+
+
+		interval = setInterval(() => {
+			if (sleepLevel < 2){
+				sleepLevel++;
+				updateDuck();
+			}
+		}, 900000); // wait 15 minutes
+		}
+	}
+
+
+function getDuckHtml(imageUrl: string, level: number): string {
+	return ` 
+		<html>
+			<body style="display:flex;justify-content:center;align-items:center;height:100vh;background-color: var(--vscode-editor-background)">
+				<img id="duck" src="${imageUrl}" style="max-width: 100%; max-height: 100%; object-fit: contain;"/>
+				<script>
+					const vscode = acquireVsCodeApi();
+					document.getElementById('duck').addEventListener('click', () => {
+					vscode.postMessage({ command: 'clicked' });
+					});
+				</script>
+			</body>
+		</html>
+	`;
 }
 
+let interval: NodeJS.Timeout;
+
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	clearInterval(interval);
+}
